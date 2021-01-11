6e0a6420c84235906d7f7ce0990897ee9db49718
Nana Janashia <nn_janashia@yahoo.com>
2021-01-11 17:28:26 +0100

    complete with db and ui edit

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -32,20 +31,38 @@ public class Application {
         Logger log = LoggerFactory.getLogger(Application.class);
         log.info("Java app started");
 
-        log.info("Connecting to the Mysql database");
         try {
-            String sqlStatement = "CREATE TABLE IF NOT EXISTS team_members(" +
-                    "member_id INT AUTO_INCREMENT PRIMARY KEY,\n" +
-                    "member_name VARCHAR(255),\n" +
-                    "member_role VARCHAR(255),\n" +
-                    "member_projects VARCHAR(255)" +
-                    ")";
             Statement stmt = dbConnection.createStatement();
-            stmt.executeUpdate(sqlStatement);
+            createTable(stmt);
+            generateData(stmt);
             stmt.close();
-            dbConnection.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+
+    }
+
+    private void createTable(Statement stmt) throws SQLException {
+        String sqlStatement = "CREATE TABLE IF NOT EXISTS team_members(" +
+                "member_id INT AUTO_INCREMENT PRIMARY KEY,\n" +
+                "member_name VARCHAR(255),\n" +
+                "member_role VARCHAR(255),\n" +
+                "member_projects VARCHAR(255)" +
+                ")";
+        stmt.executeUpdate(sqlStatement);
+    }
+
+    private void generateData(Statement stmt) throws SQLException {
+        String sqlQuery = "SELECT member_name, member_role FROM team_members";
+        ResultSet resultSet = stmt.executeQuery(sqlQuery);
+        if (!resultSet.next()) {
+            String sqlStatement = "INSERT INTO team_members(member_name, member_role)\n" +
+                    "VALUES ('Sarah', 'Full stack developer'),\n" +
+                    "('Bobby', 'React developer'),\n" +
+                    "('Ari', 'Java developer'),\n" +
+                    "('Andrea', 'DevOps engineer'),\n" +
+                    "('Bruno', 'IT operations')";
+            stmt.executeUpdate(sqlStatement);
         }
     }
 
