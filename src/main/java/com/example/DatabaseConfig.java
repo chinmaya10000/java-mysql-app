ce420ee2ecae26825e1669cf2d35a068f6b9be2e
Nana Janashia <nn_janashia@yahoo.com>
2021-01-11 17:54:23 +0100

    cleanup and host var in frontend

--- src/main/java/com/example/DatabaseConfig.java
+++ src/main/java/com/example/DatabaseConfig.java
@@ -3,7 +3,6 @@ package com.example;
 import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -12,14 +11,13 @@ import java.sql.SQLException;
 @Configuration
 public class DatabaseConfig {
 
-    // private final Environment env;
     private String user = System.getenv("DB_USER");
     private String password = System.getenv("DB_PWD");
     private String serverName = System.getenv("DB_SERVER");
     private String dbName = "team-member-projects";
     private MysqlDataSource datasource = new MysqlDataSource();
 
-    public DatabaseConfig(Environment env) {
+    public DatabaseConfig() {
         datasource.setPassword(password);
         datasource.setUser(user);
         datasource.setServerName(serverName);
