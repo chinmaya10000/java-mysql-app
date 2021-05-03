51290357c01bae041174d99e66edf18575d3a252
Nana Janashia <nn_janashia@yahoo.com>
2021-05-03 16:51:09 +0000

    Update DatabaseConfig.java

--- src/main/java/com/example/DatabaseConfig.java
+++ src/main/java/com/example/DatabaseConfig.java
@@ -13,7 +13,7 @@ public class DatabaseConfig {
 
     private String user = System.getenv("DB_USER");
     private String password = System.getenv("DB_PWD");
-    private String serverName = System.getenv("DB_SERVER");
+    private String serverName = System.getenv("DB_SERVER"); // host name, like localhost without the port
     private String dbName = System.getenv("DB_NAME");
     private MysqlDataSource datasource = new MysqlDataSource();
 
@@ -22,6 +22,7 @@ public class DatabaseConfig {
         datasource.setUser(user);
         datasource.setServerName(serverName);
         datasource.setDatabaseName(dbName);
+        //datasource.setPort(3306) default config
         //datasource.setURL("jdbc:mysql://localhost:3306/dbname");
 
 
