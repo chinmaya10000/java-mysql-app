80fdf688d4ed768a8553f9034a310a759eeaf654
chinmaya <chinmaya@gmail.com>
2024-08-03 00:01:54 +0530

    initial commit

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export IMAGE_NAME=$1
-export DOCKER_USER=$2
-export DOCKER_PWD=$3
-echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
-docker-compose up -d
+#export IMAGE_NAME=$1
+#export DOCKER_USER=$2
+#export DOCKER_PWD=$3
+#echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
+#docker-compose up -d
